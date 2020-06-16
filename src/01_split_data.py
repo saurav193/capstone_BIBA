78e879e5122724873bc36ac353f67b24fc6a61e9
sirine-chahma <54771513+sirine-chahma@users.noreply.github.com>
2020-06-16 15:42:43 -0700

    update catboost script

--- src/01_split_data.py
+++ src/01_split_data.py
@@ -2,7 +2,7 @@
 # date: 2020-05-07
 #
 """This script splits the data given as an input into training and test data.
-It saves the two sets in two separate zip files, in the folder that is given as an input.
+It saves the training and test sets in two separate zip files, in the folder that is given as an input.
 This script takes two arguments : a path/filename pointing to the data to be read in
 and a path pointing to where the cleaned data should live. 
 Usage: src/split_data.py --in_path=<in_path> --out_path=<out_path>
@@ -34,14 +34,24 @@ def main(in_path, out_path):
     train_data.to_csv(out_path + "/train_data.zip", index = False, compression=compression_opts)
     data.to_csv(out_path + "/playground_stats.zip", index = False, compression=compression_opts)
 
+    test_split(data, train_data, test_data, oupt_path)
+
     print('Split successful!')
 
     
-def test_split(data, train_data, test_data):
+def test_split(data, train_data, test_data, out_path):
+    """
+    This functions checks if the main function is able to split the data into train set and test set
+    """
+    #Check that the dimmensions match
     #Delete the `+ data.query("month == 1 & year == 2018").shape[0]` for the sum if you want to keep the January 2018 data 
     assert train_data.shape[0] + test_data.shape[0] + data.query("month == 1 & year == 2018").shape[0] + data.query("external_id == 'CA00070678'").shape[0] - 1 == data.shape[0], "The split is wrong"
     assert train_data['external_id'].unique().shape[0] == test_data['external_id'].unique().shape[0], "The numbers of unique playground in the train set and the test set are different"
     assert data['external_id'].unique().shape[0] == train_data['external_id'].unique().shape[0] + 1, "The numbers of unique playground in the train set and the original set are different"
+
+    #Check that the zip files were created
+    assert os.path.exists(out_path + "/test_data.zip"), "Train data not found in location"
+    assert os.path.exists(out_path + "/train_data.zip"), "Test data not found in location"
 
 if __name__ == "__main__":
     main(opt["--in_path"], opt["--out_path"])
--- src/04_catboost_model.py
+++ src/04_catboost_model.py
@@ -1,9 +1,12 @@
-"""This script fits a model using CatBoost, saves the model, and outputs the MAE values as a CSV file.
-Usage: src/lgbm_model.py --train=<train> --test=<test> [--out_path=<out_path>]
+"""This script fits a model using CatBoost, saves the fitted model, and outputs the MAE values as a CSV file.
+
+Usage: src/04_catboost_model.py --train=<train> --test=<test> [--model_path=<model_path>] [--out_path=<out_path>]
+
 Options:
---train=<train>            The path (including filename) of the train dataset file in zip/csv format relative to the root 
---test=<test>              The path (including filename) of the test dataset file in zip/csv format relative to the root
-[--out_path=<out_path>]    The directory name ending with "/" relative to the root to write the fitted model
+--train=<train>             The path (including filename) of the train dataset file in zip/csv format relative to the root 
+--test=<test>               The path (including filename) of the test dataset file in zip/csv format relative to the root
+[--model_path=<model_path>] The directory name ending with "/" relative to the root to write the fitted model
+[--out_path=<out_path>]     The directory name ending with "/" relative to the root to write the results as a CSV file 
 """
 
 from docopt import docopt
@@ -14,6 +17,115 @@ from catboost import CatBoostRegressor
 from joblib import dump, load
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import csv
+
+opt = docopt(__doc__)
+
+def main(train, test, model_path="src/joblib/", out_path="results/"):
+    
+ 
+    #create datasets
+    train_data = pd.read_csv(train)
+    test_data = pd.read_csv(test)
+
+    X_train = train_data.drop(columns = ['unacast_session_count'])
+    X_test = test_data.drop(columns = ['unacast_session_count'])
+    y_train = train_data['unacast_session_count']
+    y_test = test_data['unacast_session_count']
+    
+    #fitting with best params from hyperparameter optimization
+
+    catboost = CatBoostRegressor(verbose=False, loss_function='RMSE', iterations=201, depth=12) 
+
+    catboost.fit(X_train, y_train)
+    
+    y_preds_train = catboost.predict(X_train)
+    y_preds_test = catboost.predict(X_test)
+
+    #capping the negative predicted values with 0
+    y_preds_train = list(map(lambda x: 0 if x<0 else x, y_preds_train))
+    y_preds_test = list(map(lambda x: 0 if x<0 else x, y_preds_test))
+    
+    train_mae = mean_absolute_error(y_train, y_preds_train)
+    
+    test_mae = mean_absolute_error(y_test, y_preds_test)
+   
+    if model_path is None:
+        model_path = "src/joblib/catboost_model.joblib"
+    else:
+        model_path = model_path+"catboost_model.joblib"
+    
+    # save the trained model
+    dump(catboost, model_path)
+
+    # File to save results
+    if out_path is None:
+        out_file = "results/catboost_train_result.csv"
+    else:
+        out_file = out_path+"catboost_train_result.csv"
+
+    #removing existing file
+    if os.path.exists(out_file):
+        os.remove(out_file)
+
+    with open(out_file, 'w', newline='') as f: #creating file by truncating previous one
+        writer = csv.writer(f)
+        # Write the headers to the file
+        writer.writerow(['model', 'train mae', 'test mae'])
+        writer.writerow(["Catboost", train_mae, test_mae])
+    f.close()
+
+def test_fun():
+    """
+    This functions checks if the main function is able to fit a model and store the results in csv file.
+
+    """
+    train_data_loc = "data/dummy/dummy_train_data.zip"
+    test_data_loc = "data/dummy/dummy_test_data.zip"
+    main(train_data_loc, test_data_loc)
+    assert os.path.exists("src/joblib/catboost_model.joblib"), "Model dump not found in location"
+    assert os.path.exists("results/catboost_train_result.csv"), "Results file not found in location"
+
+
+if __name__ == "__main__":
+    test_fun()
+    main(opt["--train"], opt["--test"], opt["--model_path"], opt["--out_path"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 opt = docopt(__doc__)
 
@@ -31,7 +143,6 @@ def main(train, test, out_path=""):
     
     #fitting with best params from hyperparameter optimization
 
-    catboost = CatBoostRegressor(verbose=False, loss_function='RMSE', iterations=201, depth=12) 
 
     catboost.fit(X_train, y_train)
     
