b33550d4b51cf30c04c5fb9f44e9db36ea7d92aa
sirine-chahma <54771513+sirine-chahma@users.noreply.github.com>
2020-06-15 18:03:17 -0700

    work on the split data script

--- src/split_data.py
+++ src/split_data.py
@@ -5,15 +5,18 @@
 It saves the two sets in two separate csv files, in the file that is given as an input.
 This script takes two arguments : a path/filename pointing to the data to be read in
 and a path/filename pointing to where the cleaned data should live. 
-Usage: src/split_data.py
+Usage: src/split_data.py --input_file=<input_file> --ouptput_folder=<ouptput_folder>
 """
 
 #import dependencies
 import pandas as pd
+from docopt import docopt
 
-def main():
+opt = docopt(__doc__)
+
+def main(input_file, ouptput_folder):
     #Read the data and split between train and test set
-    data = pd.read_csv('data/playground_stats.csv')
+    data = pd.read_csv(input_file)
     test_data = data[(data['year']==2019) & (data['month'] > 9)]
     train_data = data[~data.index.isin(test_data.index)]
     #Comment the following line to keep the January 2018 data
@@ -27,9 +30,9 @@ def main():
 
     #Save the files
     compression_opts = dict(method='zip',archive_name='out.csv')  
-    test_data.to_csv("data/test_data.zip", index = False, compression=compression_opts)
-    train_data.to_csv("data/train_data.zip", index = False, compression=compression_opts)
-    data.to_csv("data/playground_stats.zip", index = False, compression=compression_opts)
+    test_data.to_csv(ouptput_folder + "/test_data.zip", index = False, compression=compression_opts)
+    train_data.to_csv(ouptput_folder + "/train_data.zip", index = False, compression=compression_opts)
+    data.to_csv(ouptput_folder + "/playground_stats.zip", index = False, compression=compression_opts)
 
     print('Split successful!')
 
@@ -37,7 +40,8 @@ def main():
 def test_split(data, train_data, test_data):
     #Delete the `+ data.query("month == 1 & year == 2018").shape[0]` for the sum if you want to keep the January 2018 data 
     assert train_data.shape[0] + test_data.shape[0] + data.query("month == 1 & year == 2018").shape[0] + data.query("external_id == 'CA00070678'").shape[0] - 1 == data.shape[0], "The split is wrong"
-    
+    assert train_data['external_id'].unique().shape[0] == test_data['external_id'].unique().shape[0], "The numbers of unique playground in the train set and the test set are different"
+    assert data['external_id'].unique().shape[0] == train_data['external_id'].unique().shape[0] + 1, "The numbers of unique playground in the train set and the original set are different"
 
 if __name__ == "__main__":
-    main()
+    main(opt["--input_file"], opt["--ouptput_folder"])
