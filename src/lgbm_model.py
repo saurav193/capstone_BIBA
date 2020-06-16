f37b91d35ca75761c91eb2dd3d4e7227a77720a6
saurav99 <saurav13iitr@gmail.com>
2020-06-15 19:34:31 -0700

    lgbm model tested

--- src/lgbm_model.py
+++ src/lgbm_model.py
@@ -1,14 +1,15 @@
 
-"""This script runs lgbm and stores the fitted model and output the MAE scores into a csv. 
-Usage: scripts/lgbm_model.py --train=<train> --test=<test> [--out_path=<out_path>]
+"""This script runs lgbm and stores the fitted model and output the MAE scores into a csv.
+
+Usage: src/lgbm_model.py --train=<train> --test=<test> [--out_path=<out_path>]
+
 Options:
---train=<train>            The location of the train dataset file in zip/csv format relative to the root 
---test=<test>              The location of the test dataset file in zip/csv format relative to the root
-[--out_path=<out_path>]    Path (folder_name) relative to root where to write the fitted model
+--train=<train>            The location(including filename) of the train dataset file in zip/csv format relative to the root 
+--test=<test>              The location(including filename) of the test dataset file in zip/csv format relative to the root
+[--out_path=<out_path>]    Path(folder_name) ending with "/" relative to root where to write the fitted model
 """
 
 from docopt import docopt
-import requests
 import os
 import pandas as pd
 import numpy as np
@@ -19,20 +20,21 @@ import csv
 
 opt = docopt(__doc__)
 
-def main(train, test, out_path = ''):
+def main(train, test, out_path=""):
     
+ 
     #create datasets
     train_data = pd.read_csv(train)
     test_data = pd.read_csv(test)
-    
+
     X_train = train_data.drop(columns = ['unacast_session_count'])
     X_test = test_data.drop(columns = ['unacast_session_count'])
     y_train = train_data['unacast_session_count']
     y_test = test_data['unacast_session_count']
     
     #fitting with best params from hyperparameter optimization
-    
-    lgbm = LGBMRegressor(learning_rate = 0.19981387712135354 , max_dept = 105.0, num_leaves = 300, random_state = 2020) 
+
+    lgbm = LGBMRegressor(learning_rate = 0.19981387712135354 , max_depth = 105, num_leaves = 300, random_state = 2020) 
 
     lgbm.fit(X_train, y_train)
     
@@ -43,39 +45,42 @@ def main(train, test, out_path = ''):
     y_preds_train = list(map(lambda x: 0 if x<0 else x, y_preds_train))
     y_preds_test = list(map(lambda x: 0 if x<0 else x, y_preds_test))
     
-    print('\nLGBM scores train: ')
     train_mae = mean_absolute_error(y_train, y_preds_train)
-    print("Mean absolute error: %0.3f" % mae)
     
-    print('\nLGBM scores test: ')
-    test_mae = mean_absolute_error(y_valid, y_preds_test)
-    print("Mean absolute error: %0.3f" % mae)
+    test_mae = mean_absolute_error(y_test, y_preds_test)
+   
+    if out_path is None:
+        model_path = "src/lgbm_model.joblib"
+    else:
+        model_path = out_path+"lgbm_model.joblib"
     
-        
-    # save the model to disk
-    model_path = out_path+'lgbm_model.joblib'
+    # save the trained model
     dump(lgbm, model_path)
 
     # File to save results
     out_file = 'results/lgbm_train_result.csv'
-    connection = open(out_file, 'w') #creating file by truncating previous one
-    writer = csv.writer(connection)
 
-    # Write the headers to the file
-    writer.writerow(['model', 'train mae', 'test mae'])
-    writer.writerow(["LightGBM", train_mae, test_mae])
+    #removing existing file
+    if os.path.exists("results/lgbm_train_result.csv"):
+        os.remove('results/lgbm_train_result.csv')
 
-    of_connection.close()
+    with open(out_file, 'w', newline='') as f: #creating file by truncating previous one
+        writer = csv.writer(f)
+        # Write the headers to the file
+        writer.writerow(['model', 'train mae', 'test mae'])
+        writer.writerow(["LightGBM", train_mae, test_mae])
+    f.close()
 
 def test_fun():
     """
     This functions checks if the main function is able to fit a model and store the results in csv file.
 
     """
-    train_data = "train_data.zip"
-    test_data = "valid_data.zip"
-    main(train_data, test_data)
-    assert os.path.exists("lgbm_model.joblib"), "File not found in location"
+    train_data_loc = "src/dummy_train_data.zip"
+    test_data_loc = "src/dummy_test_data.zip"
+    main(train_data_loc, test_data_loc)
+    assert os.path.exists("src/lgbm_model.joblib"), "Model dump not found in location"
+    assert os.path.exists("results/lgbm_train_result.csv"), "Results file not found in location"
 
 
 if __name__ == "__main__":
